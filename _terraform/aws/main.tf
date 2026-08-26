f79c8a01f3c95482de0b5eee1c78761a4db8847b
Davy de Haas <31407144+davydehaas98@users.noreply.github.com>
2026-08-26 19:12:47 +0200

    Update main.tf

--- _terraform/aws/main.tf
+++ _terraform/aws/main.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.10"
 
+  backend "s3" {
+    bucket         = "davydehaas-terraform-state"
+    key            = "aws/terraform.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
