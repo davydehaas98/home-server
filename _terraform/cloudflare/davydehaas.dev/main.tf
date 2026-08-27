f5db73a8c0660749f46c29a17a755aa49bc49340
Davy de Haas <31407144+davydehaas98@users.noreply.github.com>
2026-08-27 19:38:59 +0200

    Update main.tf

--- _terraform/cloudflare/davydehaas.dev/main.tf
+++ _terraform/cloudflare/davydehaas.dev/main.tf
@@ -1,5 +1,13 @@
 terraform {
-  required_version = "~> 1.10"
+  required_version = "~> 1.15"
+
+  backend "s3" {
+    bucket         = "davydehaas-terraform-state"
+    key            = "cloudflare/davydehaas.dev/terraform.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
 
   required_providers {
     cloudflare = {
@@ -7,4 +15,9 @@ terraform {
       version = "5.22.0"
     }
   }
+}
+
+provider "cloudflare" {
+  alias     = "dev"
+  api_token = var.cloudflare_api_token
 }
