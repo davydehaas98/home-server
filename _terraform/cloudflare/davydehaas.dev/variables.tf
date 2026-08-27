ad16c16aff482df6af55163e8d9c3f3f4d2b9a50
Davy de Haas <31407144+davydehaas98@users.noreply.github.com>
2026-08-27 19:38:01 +0200

    Update variables.tf

--- _terraform/cloudflare/davydehaas.dev/variables.tf
+++ _terraform/cloudflare/davydehaas.dev/variables.tf
@@ -1,11 +1,8 @@
 variable "cloudflare_api_token" {
-    description = "Cloudflare API token"
-}
-
-variable "account_id" {
-    description = "Cloudflare Account ID"
+  description = "Cloudflare API token"
 }
 
 variable "zone_id" {
-    description = "Cloudflare Zone ID"
+  description = "Cloudflare Zone ID"
+  default     = "01af14b5b71f5639198aaca16f0b38f6"
 }
