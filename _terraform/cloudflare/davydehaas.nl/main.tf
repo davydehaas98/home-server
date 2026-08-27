0ed0df954c353a25853cadc053b0bf2bcf1b1a0b
Davy de Haas <31407144+davydehaas98@users.noreply.github.com>
2026-08-27 20:25:08 +0200

    Create main.tf

---
+++ _terraform/cloudflare/davydehaas.nl/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.15"
+
+  backend "s3" {
+    bucket       = "davydehaas-terraform-state"
+    key          = "cloudflare/davydehaas.nl/terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.22.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  alias     = "nl"
+  api_token = var.cloudflare_api_token
+}
