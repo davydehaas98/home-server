646a457ab2765f60a28317665bbf0410e4715f0d
Davy de Haas <31407144+davydehaas98@users.noreply.github.com>
2026-08-27 20:09:24 +0200

    Update variables.tf

--- _terraform/cloudflare/davydehaas.nl/variables.tf
+++ _terraform/cloudflare/davydehaas.nl/variables.tf
@@ -2,6 +2,14 @@ variable "cloudflare_api_token" {
   description = "Cloudflare API token"
 }
 
+variable "ipv4_address" {
+  description = "Server IPv4 address"
+}
+
+variable "ipv6_address" {
+  description = "Server IPv6 address"
+}
+
 variable "zone_id" {
   description = "Cloudflare Zone ID"
   default     = "5ff3d3cef9bdf0f486f79115aadc9504"
